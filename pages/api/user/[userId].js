03d6987cc968ed6fba9f0ec36cd853b0aeead185
oktay.senkan <oktay.senkan@gmail.com>
2019-12-13 03:04:25 +0300

    Rest API'ye uygun olarak response kodları eklendi.

--- pages/api/user/[userId].js
+++ pages/api/user/[userId].js
@@ -3,11 +3,21 @@ import UserRepository from "../../../server/repository/UserRepository";
 export default async (req, res) => {
   const response = await UserRepository.save({
     fullName: req.query.userId
-  }).then(r => ({ data: {
-    user: r
-  }})).catch(error => ({ error: {
-    message: error
-  }}))
+  }).then(response => {
+    res.statusCode = 201;
+    return {
+      data: {
+        user: response
+      }
+    }
+  }).catch(error => {
+    res.statusCode = 404;
+    return {
+      error: {
+        message: error
+      }
+    }
+  });
 
   res.json(response);
 };
--- pages/api/users.js
+++ pages/api/users.js
@@ -1,12 +1,21 @@
 import UserRepository from "../../server/repository/UserRepository";
 
 export default async (req, res) => {
-  const response = await UserRepository.getAll()
-  .then(response => ({ data: {
-    users: response
-  }})).catch(error => ({ error: {
-    message: error
-  }}))
+  const response = await UserRepository.getAll().then(response => {
+    res.statusCode = 200;
+    return { 
+      data: {
+        users: response
+      }
+    }
+  }).catch(error => {
+    res.statusCode = 404;
+    return {
+      error: {
+        message: error
+      }
+    }
+  })
 
   res.json(response);
 };
