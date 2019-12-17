a3d671da8cf457469b9ac6f209674f83f8fd13ce
Oktay Şenkan <oktay.senkan@gmail.com>
2019-12-17 08:46:06 +0300

    Blog postlar bulunamazsa artık 404 hatası dönüyor, anasayfadaki thumbnail'lere link eklendi.

--- pages/post/[postSlug].js
+++ pages/post/[postSlug].js
@@ -1,43 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react'
+
 import fetch from 'isomorphic-unfetch';
 import Head from 'next/head';
 import Link from 'next/link';
+import Error from 'next/error'
 import ReactMarkdown from 'react-markdown';
 
 import { Container, Row, Col } from 'reactstrap';
 import { SocialMediaIcons, Header, Menu, Footer, Sidebar } from '../../components';
 import Config from '../../server/configs/config';
 
-const Post = ({ post, categories, mostReads, pages }) => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="stylesheet" href="/css/default.css" />
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
-      <link href="https://fonts.googleapis.com/css?family=News+Cycle:400,700&display=swap" rel="stylesheet"></link>
-      <link href="https://fonts.googleapis.com/css?family=Playfair+Display&display=swap" rel="stylesheet"></link>
-    </Head>
 
-    <Container>
-      <SocialMediaIcons/>
-      <Header/>
-      <Menu pages={pages}/>
-      <Row>
-        <Col md={12} lg={10}>
-          <h1>{post.title}</h1>
-          <ReactMarkdown source={post.details} />
-        </Col>
-        <Col md={12} lg={2}>
-          <Sidebar categories={categories} mostReads={mostReads}/>
-        </Col>
-      </Row>
-      <Footer/>
-    </Container>
-  </div>
-);
+export class BlogPost extends Component {
+  render() {
+    const { error, post, categories, mostReads, pages } = this.props;
+    console.log("asdasdad"+error);
+    
+    if (error) {
+      return (
+        <Error statusCode={404} />
+      )
+    }
 
-Post.getInitialProps = async ({ req, query }) => {
+    return (
+      <div>
+        <Head>
+          <title>Home</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="stylesheet" href="/css/default.css" />
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
+          <link href="https://fonts.googleapis.com/css?family=News+Cycle:400,700&display=swap" rel="stylesheet"></link>
+          <link href="https://fonts.googleapis.com/css?family=Playfair+Display&display=swap" rel="stylesheet"></link>
+        </Head>
+
+        <Container>
+          <SocialMediaIcons/>
+          <Header/>
+          <Menu pages={pages}/>
+          <Row>
+            <Col md={12} lg={10}>
+              <h1>{post.title}</h1>
+              <ReactMarkdown source={post.details} />
+            </Col>
+            <Col md={12} lg={2}>
+              <Sidebar categories={categories} mostReads={mostReads}/>
+            </Col>
+          </Row>
+          <Footer/>
+        </Container>
+      </div>
+    )
+  }
+}
+
+
+BlogPost.getInitialProps = async ({ req, res, query }) => {
   const slug = query.postSlug;
 
   const pagesResponse = await fetch(`${Config.BaseURL}/api/pages`);
@@ -49,10 +66,14 @@ Post.getInitialProps = async ({ req, query }) => {
   const mostReadsResponse = await fetch(`${Config.BaseURL}/api/posts?sort=-views&count=5`);
   const mostReadsJSON = await mostReadsResponse.json();
 
-  const postResponse = await fetch(`${Config.BaseURL}/api/posts?q={"slug":"${slug}"}`);
-  const postJSON = await postResponse.json();
-
-  return { categories: categoriesJSON.data.categories, mostReads: mostReadsJSON.data.posts, pages: pagesJSON.data.pages, post: postJSON.data.posts[0] };
+  try {
+    const postResponse = await fetch(`${Config.BaseURL}/api/posts?q={"slug":"${slug}"}`);
+    const postJSON = await postResponse.json();
+    return { categories: categoriesJSON.data.categories, mostReads: mostReadsJSON.data.posts, pages: pagesJSON.data.pages, post: postJSON.data.posts[0] };
+  } catch (error) {
+    console.log(error.toString());
+    return { error: error };
+  }
 };
 
-export default Post;
+export default BlogPost;
--- server/repository/Repository.js
+++ server/repository/Repository.js
@@ -13,5 +13,4 @@ class Repository {
   }
 }
 
-
 export default Repository;
