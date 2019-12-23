2629d69c3910d52246b0440088a44dbea30d5ce4
Oktay Şenkan <oktay.senkan@gmail.com>
2019-12-23 14:31:48 +0300

    Her sayfada çağırılan categories, most reads, page gibi istekler tek bir request'e dönüştürüldü.

--- server/utils/FetchAll.js
+++ server/utils/FetchAll.js
@@ -3,32 +3,25 @@ import { request } from 'graphql-request'
 
 class FetchAll {
   static getAll = async () => {
-    const categoriesQuery = `{
+    const query = `{
       categories {
         name
         slug
       }
-    }`;
-    const mostReadsQuery = `{
       posts(count: 5, orderBy: { field: "views", direction: "DESC" }) {
         slug
         title
       }
-    }`;
-    const pagesQuery = `{
       pages {
         slug
         name
       }
-    }`;
+    }`
 
-    const categoriesResponse = await request(Config.GraphqlURL, categoriesQuery);
-    const mostReadsResponse = await request(Config.GraphqlURL, mostReadsQuery);
-    const pagesResponse = await request(Config.GraphqlURL, pagesQuery);
-
-    const categories = categoriesResponse.categories;
-    const mostReads = mostReadsResponse.posts;
-    const pages = pagesResponse.pages;
+    const queryResponse = await request(Config.GraphqlURL, query);
+    const categories = queryResponse.categories;
+    const mostReads = queryResponse.posts;
+    const pages = queryResponse.pages;
 
     return { categories: categories, mostReads: mostReads, pages: pages };
   }
