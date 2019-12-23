7ace262be0dba846f27437fadc0891788d5ad737
Oktay Şenkan <oktay.senkan@gmail.com>
2019-12-23 15:12:54 +0300

    Graphql ortam değişkenleri ayarlandı.

--- server/configs/config.js
+++ server/configs/config.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
+
 class Config {
   static get BaseURL() {
-    const environment = process.env['NODE_ENV'];
-    if (environment === 'production') {
+    if (process.env.NODE_ENV === 'production') {
       return 'https://nextjs-blog-app.herokuapp.com';
     } else {
       return 'http://localhost:3000';
@@ -9,9 +10,8 @@ class Config {
   }
 
   static get GraphqlURL() {
-    const environment = process.env['NODE_ENV'];
-    if (environment === 'production') {
-      return 'https://nextjs-blog-app-graphql.herokuapp.com';
+    if (process.env.NODE_ENV === 'production') {
+      return process.env.GRAPHQL_URL;
     } else {
       return 'http://localhost:4000/graphql';
     }
